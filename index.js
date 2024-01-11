b5db54b38e7364d9b4350aea6d639a40f8c3dfd2
SounaVR <pinpoye21@gmail.com>
2024-01-11 10:52:28 +0100

    command aliases

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const { sep } = require("path");
 const Discord = require('discord.js-selfbot-v13');
 const fs      = require("fs");
+const { getVoiceConnection } = require('@discordjs/voice');
 
 const client = new Discord.Client({
     checkUpdate: false
 });
+
 client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
 client.on('ready', async () => {
     const load = (dir = "./Commands/") => {
@@ -21,6 +24,12 @@ client.on('ready', async () => {
 				} else {
 					console.log(`⛔ Error loading command in ${dir}${dirs}. you have a missing help.name or help.name is not a string. or you have a missing help.category or help.category is not a string`);
 					continue;
+				}
+				if (pull.help.aliases && typeof (pull.help.aliases) === "object") {
+					pull.help.aliases.forEach(alias => {
+						if (client.aliases.get(alias)) return console.warn(`⚠️ Two commands or more commands have the same aliases ${alias}`);
+						client.aliases.set(alias, pull.help.name);
+					});
 				}
 			}
 		});
@@ -45,6 +54,7 @@ client.on("messageCreate", async message => {
     if (!message.content.toLowerCase().startsWith(prefix)) return;
 
     if (client.commands.has(cmd)) command = client.commands.get(cmd);
+	else if (client.aliases.has(cmd)) command = client.commands.get(client.aliases.get(cmd));
 
     if (command) command.run(client, message, args);
 });
